0efd1691ccd314e6d937a98dc923d6a546c7bd17
Bram Vogelaar <bram@attachmentgenie.com>
2013-11-07 00:11:45 +0100

    adding initial test framwork
    Signed-off-by: Bram Vogelaar <bram@attachmentgenie.com>

--- tests/phplibTest.php
+++ tests/phplibTest.php
@@ -11,5 +11,13 @@ namespace attachmentgenie\phplib;
 
 class phplibTest extends \PHPUnit_Framework_TestCase {
 
+
+    /**
+     * Test thet true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
 }
  
